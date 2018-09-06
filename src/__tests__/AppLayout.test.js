b3f14914c11b4834315c403975c6083d0a824678
Davide Gaido <davide.0plus1@gmail.com>
2018-09-06 12:46:20 +1000

    AppLayout tests

---
+++ src/__tests__/AppLayout.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+
+import AppLayout from '../views/layouts/AppLayout';
+import SiteFooter from '../components/SiteFooter';
+
+const middlewares = [];
+const mockStore = configureStore(middlewares);
+
+describe('<AppLayout />', () => {
+  describe('renders', () => {
+    const store = mockStore({});
+    it('without crashing', () => {
+      shallow(<AppLayout />);
+    });
+    it('with children', () => {
+      const AppLayoutWithChildren = AppLayout(SiteFooter);
+      const wrapper = shallow(<AppLayoutWithChildren store={store} />).dive();
+      // expect(wrapper.contains(welcome)).to.equal(true);
+      expect(wrapper.contains(<SiteFooter />)).toEqual(true);
+    });
+  });
+});
