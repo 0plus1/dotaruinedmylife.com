e176480427f37ac65b2c609541d6789ff537f0d6
Davide Gaido <davide.0plus1@gmail.com>
2018-09-14 17:39:03 +1000

    Persist auth state in localStorage

--- src/index.js
+++ src/index.js
@@ -9,19 +9,29 @@ import ReduxPromise from 'redux-promise';
 import 'bulma/css/bulma.css';
 // Import styles order: https://github.com/facebook/create-react-app/issues/3621
 import './vendor/bulmaswatch.css';
+import registerServiceWorker from './registerServiceWorker';
 
+import { loadState, saveState } from './modules/LocalStorage';
 import Routes from './routes';
 import reducers from './reducers';
 
-import registerServiceWorker from './registerServiceWorker';
+const persistedState = loadState();
+const store = createStore(
+  reducers,
+  persistedState,
+  applyMiddleware(ReduxPromise),
+);
 
-
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+store.subscribe(() => {
+  saveState({
+    auth: store.getState().auth,
+  });
+});
 
 ReactDOM.render(
   // TODO use jsx extension for this file, will require to eject the create-react-app
   // eslint-disable-next-line react/jsx-filename-extension
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Routes />
   </Provider>,
   document.getElementById('root'),
---
+++ src/modules/LocalStorage.js
@@ -0,0 +1,22 @@
+const { localStorage } = window;
+
+export const loadState = () => {
+  try {
+    const serialisedState = localStorage.getItem('state');
+    if (serialisedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serialisedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+export const saveState = (state) => {
+  try {
+    const serialisedState = JSON.stringify(state);
+    localStorage.setItem('state', serialisedState);
+  } catch (err) {
+    // Ignore write errors.
+  }
+};
